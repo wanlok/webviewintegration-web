60d26d861ca84ba04a60d0188aefa3bfbffb0c3e
Wan Tung Lok Robert <wanlok92@gmail.com>
2020-12-05 17:01:14 +0800

    commit

--- app.js
+++ app.js
@@ -2,25 +2,30 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-var list = []
+// var list = []
 
-function getContent() {
-    var content = ""
-    for (var i = 0; i < list.length; i++) {
-        content += `<div>${list[i]}</div>`
-    }
-    return content
-}
+// function getContent() {
+//     var content = ""
+//     for (var i = 0; i < list.length; i++) {
+//         content += `<div>${list[i]}</div>`
+//     }
+//     return content
+// }
 
-app.get('/', (req, res) => {
-  res.send(`
-<html>
-    <body>
-        ${getContent()}
-    </body>
-</html>
-  `)
-})
+app.use(express.static('public'))
+
+// app.get('/', (req, res) => {
+//   res.send(`
+// <html>
+//     <head>
+//       <meta name="viewport" content="width=device-width, initial-scale=1">
+//     </head>
+//     <body>
+//         ${getContent()}
+//     </body>
+// </html>
+//   `)
+// })
 
 app.post('/', (req, res) => {
     var value = req.query.value
